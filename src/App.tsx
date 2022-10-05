b6bfe4cc2fbce26c347e0a69afa462a3609fce43
Harsh Sharma <sharmaharsh9944@gmail.com>
2022-10-05 15:45:41 +0530

    added basic ts checks for string, bool , number

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
       Start Project
+      <Greet name = {"Sanyam"} messageCount = {20} isLoggedIn = {true} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-type Props = {}
+type GreetProps = {
+    name: string
+    messageCount: number
+    isLoggedIn: boolean
+}
 
-const Greet = (props: Props) => {
+const Greet = (props: GreetProps) => {
+    
   return (
-    <div>Greet</div>
+    <div>
+        {
+        props.isLoggedIn?  <h1>Hey {props.name}, you have {props.messageCount} unread messages</h1>:<h1>Welcome Guest</h1>
+        }   
+    </div>
   )
 }
 
